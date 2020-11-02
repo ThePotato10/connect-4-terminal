a23eb9725bbe8b1f68f450c365031af39ff52982
JD Peppelman <jd.dev10@gmail.com>
2020-11-02 14:06:44 -0500

    added diagonal win logic

--- src/winner.dart
+++ src/winner.dart
@@ -41,6 +41,48 @@ bool findInColumn (List<List<String>> board, String symbol) {
   return found;
 }
 
+bool findDiagonalDown (List<List<String>> board, String symbol) {
+  // Goes from top-right to bottom-left
+  bool found = false;
+
+  for (int i = 0; i < 5; i++) { // Loops over the rows in the board where a diagonal of this type is possible
+    for (int j = 0; j < 5; j++) { // Loops over the columns where a diagonal of this is possible
+      if (board[i][j] == symbol) {
+        if (board[i + 1][j + 1] == symbol) {
+          if (board[i + 2][j + 2] == symbol) {
+            if (board[i + 3][j + 3] == symbol) {
+              found = true;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return found;
+}
+
+bool findDiagonalUp (List<List<String>> board, String symbol) {
+  // Goes from bottom-right to top-left
+  bool found = false;
+
+  for (int i = 3; i < 8; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (board[i][j] == symbol) {
+        if (board[i - 1][j + 1] == symbol) {
+          if (board[i - 2][j + 2] == symbol) {
+            if (board[i - 3][j + 3] == symbol) {
+              found = true;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return found;
+}
+
 bool determineWinner (bool player) {
   String symbol = (player) ? "X" : "O";
   bool found = false;
@@ -52,6 +94,14 @@ bool determineWinner (bool player) {
   }
 
   if (findInColumn(gameBoard, symbol)) {
+    found = true;
+  }
+
+  if (findDiagonalDown(gameBoard, symbol)) {
+    found = true;
+  }
+
+  if (findDiagonalUp(gameBoard, symbol)) {
     found = true;
   }
 
